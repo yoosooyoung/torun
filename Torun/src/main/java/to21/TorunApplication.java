8dd6dfb9b7e17b09764b4ad12f6058e8516fca50
yoosooyoung <wndghk202@naver.com>
2022-07-05 18:17:59 +0900

    프로젝트 세팅

---
+++ Torun/src/main/java/to21/TorunApplication.java
@@ -0,0 +1,13 @@
+package to21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorunApplication.class, args);
+	}
+
+}
---
+++ Torun/src/test/java/to21/TorunApplicationTests.java
@@ -0,0 +1,13 @@
+package to21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TorunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
