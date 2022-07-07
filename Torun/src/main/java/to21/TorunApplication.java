d81200dfad5ee9204862b18c090e631b7cf951f2
tpgns1106 <qudwls7042@gmail.com>
2022-07-07 22:45:50 +0900

    jsp 경로 설정 및 index.jsp 테스트

--- Torun/src/main/java/to21/TorunApplication.java
+++ Torun/src/main/java/to21/TorunApplication.java
@@ -2,16 +2,13 @@ package to21;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class TorunApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TorunApplication.class, args); 
-	}
-	public static void main3(String[] args) {
 		SpringApplication.run(TorunApplication.class, args);
-		System.out.println("Torun");
-		System.out.println("hello torun");
 	}
 }
